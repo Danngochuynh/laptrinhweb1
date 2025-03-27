822392044f1091692ccf0b6763e625e120396ca3
ngocdan <132124254+Danngochuynh@users.noreply.github.com>
2025-03-27 22:16:11 +0700

    add phone andres

---
+++ example-app/resources/views/crud_user/update.blade.php
@@ -0,0 +1,61 @@
+@extends('dashboard')
+
+@section('content')
+    <main class="signup-form">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h3 class="card-header text-center">Update User</h3>
+                        <div class="card-body">
+
+                            @if(session('success'))
+                                <div class="alert alert-success">
+                                    {{ session('success') }}
+                                </div>
+                            @endif
+                            @if(session('error'))
+                                <div class="alert alert-danger">
+                                    {{ session('error') }}
+                                </div>
+                            @endif
+
+                            <form action="{{ route('user.postUpdateUser') }}" method="POST">
+                                @csrf
+                                <input name="id" type="hidden" value="{{ $user->id }}">
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Name" id="name" class="form-control" name="name"
+                                           value="{{ old('name', $user->name) }}" required autofocus>
+                                    @if ($errors->has('name'))
+                                        <span class="text-danger">{{ $errors->first('name') }}</span>
+                                    @endif
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Email" id="email_address" class="form-control"
+                                           name="email" value="{{ old('email', $user->email) }}" required>
+                                    @if ($errors->has('email'))
+                                        <span class="text-danger">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <input type="password" placeholder="New Password (Optional)" id="password" class="form-control"
+                                           name="password">
+                                    @if ($errors->has('password'))
+                                        <span class="text-danger">{{ $errors->first('password') }}</span>
+                                    @endif
+                                </div>
+
+                                <div class="d-grid mx-auto">
+                                    <button type="submit" class="btn btn-dark btn-block">Update</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
