fa2b330ff63ea5a1a3a45e3227b07061c5c7abcb
ngocdan <132124254+Danngochuynh@users.noreply.github.com>
2025-04-03 16:54:27 +0700

    Nhúng_html

--- example-app/resources/views/crud_user/update.blade.php
+++ example-app/resources/views/crud_user/update.blade.php
@@ -1,75 +1,70 @@
 @extends('dashboard')
 
 @section('content')
-<main class="signup-form">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="card">
-                    <h3 class="card-header text-center">Update User</h3>
-                    <div class="card-body">
-
-                        @if(session('success'))
-                        <div class="alert alert-success">
-                            {{ session('success') }}
-                        </div>
-                        @endif
-                        @if(session('error'))
-                        <div class="alert alert-danger">
-                            {{ session('error') }}
-                        </div>
-                        @endif
-
-                        <form action="{{ route('user.postUpdateUser') }}" method="POST">
-                            @csrf
-                            <input name="id" type="hidden" value="{{ $user->id }}">
-
-                            <div class="form-group mb-3">
-                                <input type="text" placeholder="Name" id="name" class="form-control" name="name"
-                                    value="{{ old('name', $user->name) }}" required autofocus>
-                                @if ($errors->has('name'))
+    <!-- Main -->
+    <main>
+        <div class="container mt-5 d-flex justify-content-center">
+            <div class="border border-2 border-dark p-5" style="max-width: 500px; width: 100%">
+                <h1 class="text-center fs-5 mb-4">Màn hình cập nhật</h1>
+                <form action="{{ route('user.postUpdateUser') }}" method="POST">
+                    @csrf
+                    <input name="id" type="hidden" value="{{ $user->id }}">
+                    <div class="mb-3 row align-items-center">
+                        <label for="username" class="col-sm-3 form-label">Username</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control p-1 border border-dark rounded-0" name="name"
+                                value="{{ $user->name }}" id="username" required autofocus />
+                            @if ($errors->has('name'))
                                 <span class="text-danger">{{ $errors->first('name') }}</span>
-                                @endif
-                            </div>
-
-                            <div class="form-group mb-3">
-                                <input type="text" placeholder="Email" id="email_address" class="form-control"
-                                    name="email" value="{{ old('email', $user->email) }}" required>
-                                @if ($errors->has('email'))
-                                <span class="text-danger">{{ $errors->first('email') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <input type="text" placeholder="Phone" id="phone" class="form-control"
-                                    name="phone" value="{{ old('phone', $user->phone) }}" required>
-                                @if ($errors->has('phone'))
-                                <span class="text-danger">{{ $errors->first('phone') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <input type="text" placeholder="Address" id="address" class="form-control"
-                                    name="address" value="{{ old('address', $user->address) }}" required>
-                                @if ($errors->has('address'))
-                                <span class="text-danger">{{ $errors->first('address') }}</span>
-                                @endif
-                            </div>
-
-                            <div class="form-group mb-3">
-                                <input type="password" placeholder="New Passwords (Optional)" id="password" class="form-control"
-                                    name="password">
-                                @if ($errors->has('password'))
-                                <span class="text-danger">{{ $errors->first('password') }}</span>
-                                @endif
-                            </div>
-
-                            <div class="d-grid mx-auto">
-                                <button type="submit" class="btn btn-dark btn-block">Update</button>
-                            </div>
-                        </form>
+                            @endif
+                        </div>
                     </div>
-                </div>
+                    <div class="mb-3 row align-items-center">
+                        <label for="email" class="col-sm-3 form-label">Email</label>
+                        <div class="col-sm-9">
+                            <input type="email" class="form-control p-1 border border-dark rounded-0" name ="email"
+                                value="{{ $user->email }}" id="email" required />
+                            @if ($errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="mb-3 row align-items-center">
+                        <label for="phone" class="col-sm-3 form-label">Phone</label>
+                        <div class="col-sm-9">
+                            <input type="tel" class="form-control p-1 border border-dark rounded-0" name ="phone"
+                                value="{{ $user->phone }}" id="phone" required />
+                            @if ($errors->has('phone'))
+                                <span class="text-danger">{{ $errors->first('phone') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="mb-3 row align-items-center">
+                        <label for="address" class="col-sm-3 form-label">Address</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control p-1 border border-dark rounded-0" name ="address"
+                                value="{{ $user->address }}" id="address" />
+                            @if ($errors->has('address'))
+                                <span class="text-danger">{{ $errors->first('address') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="mb-3 row align-items-center">
+                        <label for="password" class="col-sm-3 form-label">Mật khẩu</label>
+                        <div class="col-sm-9">
+                            <input type="password" class="form-control p-1 border border-dark rounded-0" name="password"
+                                id="password" placeholder="Nhập mật khẩu mới nếu muốn thay đổi" />
+                            @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center mt-5">
+                        <a class="ms-auto me-4 text-decoration-none" href="{{ route('user.list') }}">Quay lại</a>
+                        <button type="submit" class="btn btn-primary">Lưu</button>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 @endsection
